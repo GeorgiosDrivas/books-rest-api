7eac31bdeed3a89e8ae487a9e6a3b806fd278238
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-10 15:13:22 +0300

    Simplify BookService

--- src/main/java/com/georgiosDrivas/books/service/BookService.java
+++ src/main/java/com/georgiosDrivas/books/service/BookService.java
@@ -1,18 +1,40 @@
 package com.georgiosDrivas.books.service;
 
 import com.georgiosDrivas.books.model.BookModel;
+import com.georgiosDrivas.books.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookService {
-    BookModel save(BookModel book);
+@Service
+public class BookService {
 
-    Optional<BookModel> findById(String isbn);
+    private final BookRepository bookRepository;
 
-    List<BookModel> listAllBooks();
+    @Autowired
+    public BookService(final BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
 
-    boolean isBookExists(BookModel book);
+    public BookModel save(final BookModel book) {
+        return bookRepository.save(book);
+    }
 
-    void deleteBookById(String isbn);
+    public Optional<BookModel> findById(String isbn) {
+        return bookRepository.findById(isbn);
+    }
+
+    public List<BookModel> listAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public boolean isBookExists(BookModel book) {
+        return bookRepository.existsById(book.getIsbn());
+    }
+
+    public void deleteBookById(String isbn) {
+        bookRepository.deleteById(isbn);
+    }
 }
